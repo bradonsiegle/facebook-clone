9493004c7e94b5a2454482796db8df438ff733e8
Bradon Siegle <bradonsiegle@gmail.com>
2022-08-29 17:46:36 -0600

    login page skeleton

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,3 +1,33 @@
+import './login.scss';
+import { Formik, Form } from 'formik';
+
 export default function Login() {
-	return <div>Login</div>;
+	return (
+		<div className='login'>
+			<div className='login__wrapper'>
+				<div className='login__wrap'>
+					<div className='login__1'>
+						<img src='../../icons/facebook.svg' alt='' />
+						<span>
+							Facebook Clone helps you connect and share with the people in your
+							life.
+						</span>
+					</div>
+					<div className='login__2'>
+						<div className='login__2__wrap'>
+							<Formik>
+								{(formik) => (
+									<Form>
+										<input type='text' />
+										<input type='text' />
+										<button type='submit'>Log In</button>
+									</Form>
+								)}
+							</Formik>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
